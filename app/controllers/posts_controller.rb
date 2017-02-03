c79e1374f6936b44dc9461ee7830e2a0e83d0d3b
rudis <rudislpp@gmail.com>
2017-02-03 17:19:19 -0200

    Implemented edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
@@ -19,16 +19,28 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
+  def edit
+
   end
 
+  def update
+    if @post.update(post_params)
+    redirect_to @post, notice: 'Your post was created!'
+    else
+    render :edit
+    end
+  end
+
+  def show
+
+  end
   private
 
-    def post_params
-      params.require(:post).permit(:date, :rationale)
-    end
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
 
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
