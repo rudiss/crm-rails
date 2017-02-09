550844ad03048990089e3af43329d2c48e526529
Rudinei Silva <rudislpp@gmail.com>
2017-02-09 09:11:38 -0200

    fix admin area

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -1,12 +1,6 @@
 require "administrate/base_dashboard"
 
-class UserDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
+class AdminUserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     posts: Field::HasMany,
     id: Field::Number,
@@ -24,20 +18,12 @@ class UserDashboard < Administrate::BaseDashboard
     type: Field::String,
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :posts,
     :id,
     :email,
-    :type
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :posts,
     :id,
@@ -54,13 +40,11 @@ class UserDashboard < Administrate::BaseDashboard
     :type,
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :email,
     :password,
     :first_name,
     :last_name,
+    :type,
   ].freeze
 end
