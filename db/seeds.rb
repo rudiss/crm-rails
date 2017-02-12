3239815d6eb68be444a28ef4e3e3f18ad27474fe
Rudinei Silva <rudislpp@gmail.com>
2017-02-12 16:26:11 -0200

    status post and admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 @user = User.create(email: "teste@teste.com", password: "123321", password_confirmation: "123321", first_name: "Jhon", last_name: "Snow")
 
 puts "1 User created"
+
+ AdminUser.create(email: "admin@test.com", password: "123321", password_confirmation: "123321", first_name: "Amin", last_name: "Jedi")
+
+puts "1 AdminUser  created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id )
 end
