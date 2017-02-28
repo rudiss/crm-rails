96cdcd8cb262148d08dd986c72310f574462552e
Rudinei Silva <rudislpp@gmail.com>
2017-02-28 18:29:38 -0300

    update gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 @user = User.create(email: "teste@teste.com",
-  password: "123321",
-  password_confirmation: "123321",
-  first_name: "Jhon",
-  last_name: "Snow",
-  phone: "5553984244819")
+                    password: "123321",
+                    password_confirmation: "123321",
+                    first_name: "Jhon",
+                    last_name: "Snow",
+                    phone: "5553984244819")
 
 puts "1 User created"
 
 AdminUser.create(email: "admin@test.com",
-  password: "123321",
-  password_confirmation: "123321",
-  first_name: "Anakin",
-  last_name: "Skywalker",
-  phone: "5553984244819")
+                password: "123321",
+                password_confirmation: "123321",
+                first_name: "Anakin",
+                last_name: "Skywalker",
+                phone: "5553984244819")
 
 puts "1 AdminUser  created"
 
 100. times do |post|
   Post.create!(date: Date.today,
-    rationale: "#{post} rationale content",
-    user_id: @user.id,
-    overtime_request: 2.5)
+              rationale: "#{post} rationale content",
+              user_id: @user.id,
+              overtime_request: 2.5)
 end
 puts "100 post has been created!"
 
 100. times do |audit_log|
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: Date.today - 6.days)
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 AuditLogs has been created!"
